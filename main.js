bbece44310ec3a2a674b7324ea716e60358795b4
malik-jahangir <awan.jahangir.malik@gmail.com>
2023-05-01 15:14:01 +0500

    added projects

--- main.js
+++ main.js
@@ -191,45 +191,45 @@ gsap.to(".h-about", {
 
 
   //////////screenshots
-  gsap.to(".screenshot-t", {
+  gsap.to(".screenshot-thejob", {
     scrollTrigger:{
-        trigger:'.screenshot-t',
+        trigger:'.screenshot-thejob',
         // markers: true,
-        start: "12% 58%", // when the top of the trigger hits the top of the viewport
-      end: "0% 12%", // end after scrolling 500px beyond the start
+        start: "12% 50%", // when the top of the trigger hits the top of the viewport
+      end: "0% 0%", // end after scrolling 500px beyond the start
         // pin: true,   // pin the trigger element while active,
         scrub: 2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     }, // start the animation when ".box" enters the viewport (once)
-    y: -180,
+    y: -160,
     duration:3,
     ease: "none",
 
 
   });
-  gsap.to(".screenshot-tab-t", {
+  gsap.to(".screenshot-tab-thejob", {
     scrollTrigger:{
-        trigger:'.screenshot-tab-t',
+        trigger:'.screenshot-tab-thejob',
         // markers: true,
         start: "10% 50%", // when the top of the trigger hits the top of the viewport
-      end: "15% 0%", // end after scrolling 500px beyond the start
+      end: "15% 10%", // end after scrolling 500px beyond the start
         // pin: true,   // pin the trigger element while active,
         scrub: 2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     }, // start the animation when ".box" enters the viewport (once)
-    y: -188,
+    y: -170,
     duration:3,
     ease: "none",
 
   });
-  gsap.to(".screenshot-phone-t", {
+  gsap.to(".screenshot-phone-thejob", {
     scrollTrigger:{
-        trigger:'.screenshot-phone-t',
+        trigger:'.screenshot-phone-thejob',
         // markers: true,
-        start: "20% 70%", // when the top of the trigger hits the top of the viewport
-      end: "6% 0%", // end after scrolling 500px beyond the start
+        start: "20% 50%", // when the top of the trigger hits the top of the viewport
+      end: "6% 10%", // end after scrolling 500px beyond the start
         // pin: true,   // pin the trigger element while active,
         scrub: 2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     }, // start the animation when ".box" enters the viewport (once)
-    y: -240,
+    y: -96,
     duration:3,
     ease: "none",
 
@@ -282,8 +282,6 @@ gsap.to(".h-about", {
 
 
   });
-
-
   //////////screenshots
   gsap.to(".screenshot-c", {
     scrollTrigger:{
@@ -330,7 +328,105 @@ gsap.to(".h-about", {
 
   });
 
- /////////////////////////////common 
+
+
+  ///////////////////////project 4
+  gsap.to(".project-mockup4", {
+    scrollTrigger:{
+        trigger:'.project-mockup4',
+        // markers: true,
+        start: "10% top", // when the top of the trigger hits the top of the viewport
+      end: "+=400", // end after scrolling 500px beyond the start
+        // pin: true,   // pin the trigger element while active,
+        scrub: 3, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    }, // start the animation when ".box" enters the viewport (once)
+    y: "-40",
+    duration:2,
+    // ease: "none",
+
+
+  });
+  gsap.to(".project-mockup4-tab", {
+    scrollTrigger:{
+        trigger:'.project-mockup4-tab',
+        // markers: true,
+        start: "50% 80%", // when the top of the trigger hits the top of the viewport
+      end: "40% 20%", // end
+        // pin: true,   // pin the trigger element while active,
+        scrub: 2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    }, // start the animation when ".box" enters the viewport (once)
+    y: "-100",
+    duration:2,
+    // ease: "none",
+
+
+  });
+  gsap.to(".project-mockup4-phone", {
+    scrollTrigger:{
+        trigger:'.project-mockup4-phone',
+        // markers: true,
+        start: "60% bottom", // when the top of the trigger hits the top of the viewport
+      end: "+=600", // end after scrolling 500px beyond the start
+        // pin: true,   // pin the trigger element while active,
+        scrub: 3, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    }, // start the animation when ".box" enters the viewport (once)
+    y: "-80",
+    duration:2,
+    // ease: "none",
+
+
+  });
+
+
+  //////////screenshots
+  gsap.to(".screenshot-trava", {
+    scrollTrigger:{
+        trigger:'.screenshot-trava',
+        // markers: true,
+        start: "12% 50%", // when the top of the trigger hits the top of the viewport
+      end: "20% 12%", // end after scrolling 500px beyond the start
+        // pin: true,   // pin the trigger element while active,
+        scrub: 2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    }, // start the animation when ".box" enters the viewport (once)
+    y: -240,
+    duration:3,
+    ease: "none",
+
+
+  });
+  gsap.to(".screenshot-tab-trava", {
+    scrollTrigger:{
+        trigger:'.screenshot-tab-trava',
+        // markers: true,
+        start: "14% 50%", // when the top of the trigger hits the top of the viewport
+      end: "80% 10%", // end after scrolling 500px beyond the start
+        // pin: true,   // pin the trigger element while active,
+        scrub: 2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    }, // start the animation when ".box" enters the viewport (once)
+    y: -180,
+    duration:3,
+    ease: "none",
+
+  });
+  gsap.to(".screenshot-phone-trava", {
+    scrollTrigger:{
+        trigger:'.screenshot-phone-trava',
+        // markers: true,
+        start: "15% 40%", // when the top of the trigger hits the top of the viewport
+      end: "6% 20%", // end after scrolling 500px beyond the start
+        // pin: true,   // pin the trigger element while active,
+        scrub: 2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+    }, // start the animation when ".box" enters the viewport (once)
+    y: -70,
+    duration:3,
+    ease: "none",
+
+
+  });
+
+
+
+ /////////////////////////////all projects title and description
   const opToggle = function(el){
       el.classList.toggle('op100');
   }
@@ -338,10 +434,12 @@ gsap.to(".h-about", {
   const overlay1 = document.querySelectorAll('#overlay1');
   const overlay2 = document.querySelectorAll('#overlay2');
   const overlay3 = document.querySelectorAll('#overlay3');
+  const overlay4 = document.querySelectorAll('#overlay4');
   
   const projectDescription1 = document.querySelector('.project-description1');
   const projectDescription2 = document.querySelector('.project-description2');
   const projectDescription3 = document.querySelector('.project-description3');
+  const projectDescription4 = document.querySelector('.project-description4');
   const projectMockup = document.querySelector('.project-mockups')
   ScrollTrigger.create({
     trigger: ".project1",
@@ -376,6 +474,19 @@ gsap.to(".h-about", {
     onToggle: self => {
       opToggle(projectDescription3)
       overlay3.forEach((e)=>{
+        // console.log(e)
+        e.classList.toggle('overlay')
+      })
+    }
+  });
+
+  ScrollTrigger.create({
+    trigger: ".project4",
+    start: "top center",
+    end: "+=1800",
+    onToggle: self => {
+      opToggle(projectDescription4)
+      overlay4.forEach((e)=>{
         // console.log(e)
         e.classList.toggle('overlay')
       })
